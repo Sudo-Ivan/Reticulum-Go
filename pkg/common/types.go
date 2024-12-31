fe96ab47d27a74c6bf8e24873665bb102a3c9537
Sudo-Ivan <ivan@ivanryan.dev>
2024-12-31 11:43:22 -0600

    update common packages

--- pkg/common/types.go
+++ pkg/common/types.go
@@ -8,7 +8,7 @@ import (
 type TransportMode byte
 type PathStatus byte
 
-// Common structs
+// Path represents routing information for a destination
 type Path struct {
 	Interface   NetworkInterface
 	LastSeen    time.Time
@@ -20,20 +20,55 @@ type Path struct {
 // Common callbacks
 type ProofRequestedCallback func([]byte, []byte)
 type LinkEstablishedCallback func(interface{})
+type PacketCallback func([]byte, interface{})
 
-// Request handler
+// RequestHandler manages path requests and responses
 type RequestHandler struct {
 	Path              string
 	ResponseGenerator func(path string, data []byte, requestID []byte, linkID []byte, remoteIdentity interface{}, requestedAt int64) []byte
 	AllowMode         byte
 	AllowedList       [][]byte
 }
+
+// Interface types
 type InterfaceMode byte
 type InterfaceType byte
 
-// PacketCallback defines the function signature for packet handling
-type PacketCallback func([]byte, interface{})
-
+// RatchetIDReceiver holds ratchet ID information
 type RatchetIDReceiver struct {
-    LatestRatchetID []byte
+	LatestRatchetID []byte
+}
+
+// NetworkStats holds interface statistics
+type NetworkStats struct {
+	BytesSent       uint64
+	BytesReceived   uint64
+	PacketsSent     uint64
+	PacketsReceived uint64
+	LastUpdated     time.Time
+}
+
+// LinkStatus represents the current state of a link
+type LinkStatus struct {
+	Established bool
+	LastSeen    time.Time
+	RTT         time.Duration
+	Quality     float64
+	Hops        uint8
+}
+
+// PathRequest represents a path discovery request
+type PathRequest struct {
+	DestinationHash []byte
+	Tag             []byte
+	TTL             int
+	Recursive       bool
+}
+
+// PathResponse represents a path discovery response
+type PathResponse struct {
+	DestinationHash []byte
+	NextHop         []byte
+	Hops            uint8
+	Tag             []byte
 }
